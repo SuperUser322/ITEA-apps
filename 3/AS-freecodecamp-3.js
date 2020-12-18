98e327e39a45a836d937028e760f1799aa692bcb
Alex Martin <borntorunthecopsdonotcatch@gmail.com>
2020-12-18 14:39:56 +0200

    added 3rd app and 6 another exercises on codecamp

---
+++ 3/AS-freecodecamp-3.js
@@ -0,0 +1,74 @@
+﻿//11 декларативное, если делать императивно то по идее оно будет работать быстрее и так же будет очень многоэтажным
+const titleCase = (str) => str = str.toLowerCase().replace(/(^|\s)\S/g, letter => letter.toUpperCase());
+
+titleCase("I'm a little tea pot");
+
+
+//12 декларативное
+function frankenSplice(arr1, arr2, n) {
+    let newArr = arr2.slice();
+    newArr.splice(n, 0, arr1);
+    return newArr.flat();       //.flat() это такой метод, который убирает вложенности, уже им пользовался
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+//13 декларативное
+const bouncer = (arr) => arr.filter(Boolean);
+
+bouncer([7, "ate", "", false, 9]);
+
+
+//14 императивное
+function getIndexToIns(arr, num) {
+    let number = 0;
+    if (arr.length === 0) {
+        return 0;
+    }
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] < num) {
+            number += 1;
+        }
+    }
+    return number
+}
+
+getIndexToIns([40, 60], 50);
+
+//14    вроде даже так можно
+function getIndexToIns(arr, num) {
+    let number = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] < num) {
+            number += 1;
+        }
+    }
+    return number
+}
+
+getIndexToIns([40, 60], 50);
+
+
+//15
+function mutation(arr) {
+    let check = arr[1].toLowerCase();
+    for (var i = 0; i < check.length; i++) {
+        if (arr[0].toLowerCase().indexOf(check[i]) < 0) return false;
+    }
+    return true;
+}
+
+mutation(["hello", "hey"]);
+
+
+//16
+function chunkArrayInGroups(arr, size) {
+    var arr1 = [];
+    while (arr.length) {
+        arr1.push(arr.splice(0, size));
+    }
+    return arr1;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
---
+++ 3/Weather v2.0/script.js
@@ -0,0 +1,101 @@
+﻿window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+recognition.lang = 'en-US';
+recognition.interimResults = true;
+
+var words = getElement('.words');
+var p = document.createElement('p');
+words.appendChild(p);
+
+var city = getElement('#city');
+var humidity = getElement('#humidity');
+var pressure = getElement('#pressure');
+var temperature = getElement('#temperature');
+var windSpeed = getElement('#wind-speed');
+var weatherIcon = getElement('.weather-icon');
+var getWeatherButton = getElement('#get-weather');
+var loader = getElement('.loader');
+var hiddenPart = getElement('.hidden-part');
+var weatherWrapper = getElement('.weather-wrapper');
+
+function getElement(selector) {
+    return document.querySelector(selector);
+}
+
+function getLocationCoords(cityName) {
+    getWeatherButton.style.display = 'none';
+    hiddenPart.style.visibility = 'visible';
+
+    loader.style.display = 'block';
+    if (navigator.geolocation) {
+        setTimeout(() => { loader.style.display = 'none' }, 1000);
+        getWeatherData(cityName);
+    } else {
+        alert('Your browser does not support Navigator API');
+    }
+}
+
+function getWeatherData(q) {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + q + '&appid=08d1316ba8742c08076e7425c28c2614')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayData(data);
+        })
+}
+
+function displayData(data) {
+
+    console.log(data);
+    city.innerText = data.name;
+    humidity.innerText = data.main.humidity + ' %';
+    pressure.innerText = hPaTommHg(data.main.pressure) + ' мм рт ст';
+    temperature.innerText = kToC(data.main.temp) + 'ºC';
+    windSpeed.innerText = data.wind.speed + ' м/с';
+    weatherIcon.innerHTML = ("<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png'>");
+}
+
+function kToC(k) {      //из кельвинов в цельсии
+    return Math.round(k - 273.15);
+}
+
+function hPaTommHg(hPa) {       //из гектопаскалей в миллиметры ртутного столба
+    return Math.round(hPa * 0.75006375541921);
+}
+
+function checkingСommands(commands) {         //функция с разложением комманд
+    console.log(commands);          //на всякий случай
+    let coms = commands.split(' ');
+    let cityName = coms[0];
+    cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+    let start = coms[1];
+    if (start === 'go') {
+        return getLocationCoords(cityName);
+    } else console.log('Error: Unknown command.')
+}
+
+recognition.addEventListener('result', function (event) {
+    p.innerText = Array
+        .from(event.results)
+        .map(function (result) {
+            return result[0];
+        })
+        .map(function (result) {
+            return result.transcript;
+        })                                    //ГДЕ-ТА ТУТА ОТЧЕБУЧИВАНИЕ ЧУЧИ(ЛОГИКА) С "ГОРОД" и "ГОУ" (комманды)
+    if (event.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+        return weatherWrapper.style.visibility = 'visible';
+    }
+    checkingСommands(p.innerText);
+    setTimeout(() => { words.remove(); }, 1000);
+});
+
+
+recognition.start();
+
+getWeatherButton.addEventListener('click', getLocationCoords);
+
